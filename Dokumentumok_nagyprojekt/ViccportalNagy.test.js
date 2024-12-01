54f9442a68eb38bbecebb37d02ec4d17db82922d
Helli Milán <milan.helli2003@gmail.com>
2024-12-01 18:11:05 +0100

    Értékelés kérése tesztelése

--- Dokumentumok_nagyprojekt/ViccportalNagy.test.js
+++ Dokumentumok_nagyprojekt/ViccportalNagy.test.js
@@ -17,4 +17,16 @@ function displayTopJokes() {
         li.appendChild(rateButton);
         topJokesList.appendChild(li);
     });
+    // 2. Teszt: Értékelés kérése
+    function promptRating(jokeId) {
+        const rating = parseInt(prompt('Add meg az értékelést (1-5):'), 10);
+
+        if (rating >= 1 && rating <= 5) {
+            updateJokeRating(jokeId, rating);
+            alert('Értékelés sikeresen frissítve!');
+            displayTopJokes();
+        } else {
+            alert('Érvénytelen értékelés! Kérlek, adj meg egy értéket 1 és 5 között.');
+        }
+    }
 }
